eba3c116cfab959b3c0e015d965be03e3faba90b
SCHMITT <nicolas.schmitt@isen.yncrea.fr>
2019-11-07 15:28:58 +0100

    Character 2
    Ajout de la méthode levelUp() dans la class character

--- Character/Character.py
+++ Character/Character.py
@@ -15,3 +15,30 @@ class Character():
         self.armor = armor      # Armor point, reduced incoming damage by a percentage
         self.level = level      # Level, each leave increase a small of the characteristics above
         self.xp = xp            # Experience bar, when filled, increase cureent level by one
+
+
+    """ Methods """
+
+    """ When the character earned enought xp, he gained a level """
+    def levelUp(self):
+        self.level += 1
+
+        # These stats are doubled (arbitrary)
+        self.health *= 2
+        self.shield *= 2
+        self.mana *= 2
+        self.damageMin *= 2
+        self.damageMax *= 2
+
+        # These stats are doubled (arbitrary) whithout exceed 20 % (of the stat)
+        if(self.dodge < 10):
+            self.dodge *= 2
+
+        if(self.parry < 10):
+            self.parry *= 2
+        
+        if(self.criticalHit < 10):
+            self.criticalHit *= 2
+        
+        if(self.armor < 10):
+            self.armor *= 2
