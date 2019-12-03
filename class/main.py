480cbe6f6d842de46cc4a9197a6c2b2efde99a3d
SCHMITT <nicolas.schmitt@isen.yncrea.fr>
2019-12-04 00:36:41 +0100

    Character & Inventory
    Tout dans le dossier class pour pas se faire chier
    main permet de faire les tests

---
+++ class/main.py
@@ -0,0 +1,14 @@
+import Character
+import Inventory
+
+def main():
+
+    print("-------------------------Test Character----------------------------")
+    perso = Character.Character(name = "Nico", health=100, shield=50, dodge=1, parry=1, criticalHit=1, mana=10, damageMin=1, damageMax=10, armor=0, level=1, xp=0)
+    print(perso.showInfo())
+
+    print("-------------------------Test Inventory----------------------------")
+    inventory = Inventory.Inventory(objects=[], gold=0, leftHand=None, rigthHand=None, jewel1=None, jewel2=None, headArmor=None, chestArmor=None, pantsArmor=None, armsArmor=None, legsArmor=None)
+    print(inventory.showInfo())
+
+main()
