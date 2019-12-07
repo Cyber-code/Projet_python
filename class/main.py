42e7a0deb533454362c7db96681ad480e95ed96d
SCHMITT <nicolas.schmitt@isen.yncrea.fr>
2019-12-07 16:38:03 +0100

    generer Weapon

--- class/main.py
+++ class/main.py
@@ -1,6 +1,6 @@
 from Inventory import Inventory
 from Consumable import Consumable
-from Weapon import Weapon
+from Weapon import *
 from Player import Player
 from Jewels import Jewels
 from Armor import Armor
@@ -11,5 +11,26 @@ def main():
     player = Player(name="Nico")
 
     print(player.showInfo())
+
+    weapon = generateWoodenSword()
+    print(weapon.showInfo())
+
+    weapon1 = generateGoldenSword()
+    print(weapon1.showInfo())
+
+    weapon2 = generateStoneSword()
+    print(weapon2.showInfo())
+
+    weapon3 = generateIronSword()
+    print(weapon3.showInfo())
+
+    weapon4 = generateDiamondSword()
+    print(weapon4.showInfo())
+
+    weapon5 = generateBow()
+    print(weapon5.showInfo())
+
+    weapon6 = generateCrossbow()
+    print(weapon6.showInfo())
 
 main()
